b8fa3f2670843532eb6a50af20c077396f9686eb
Vishal Tanna <vishal.tanna2010@gmail.com>
2022-07-19 13:49:33 +0530

    Remove Dummy Data

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -5,31 +5,6 @@ const HttpError = require('../models/http-error');
 const getCoordsForAddress = require('../util/locations');
 const Place = require('../models/place');
 
-let DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'One of the most famous sky scrapers in the world!',
-        address: '20 W 34th St, New York, NY 10001',
-        location: {
-            lat: 40.7484405,
-            lng: -73.9878584
-        },
-        creator: 'u1'
-    },
-    {
-        id: 'p2',
-        title: 'Empire State Building',
-        description: 'One of the most famous sky scrapers in the world!',
-        address: '20 W 34th St, New York, NY 10001',
-        location: {
-            lat: 40.7484405,
-            lng: -73.9878584
-        },
-        creator: 'u2'
-    }
-];
-
 const getPlaceById = async (req,res,next) => {
     const placeId = req.params.pid;
 
